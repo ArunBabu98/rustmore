3039df3524c721be5be89515d7c071ec3e5ed98b
Arun Xena <arun.b@tenshilabs.com>
2026-03-02 23:48:29 +0530

    read names.txt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+mod bigram;
+
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+fn main() -> std::io::Result<()> {
+    let file = File::open("./assets/names.txt")?;
+    let reader = BufReader::new(file);
+    let names: Vec<String> = reader.lines().filter_map(Result::ok).collect();
+    println!("{:?}",&names[..10]);
+    println!("Total number of names: {}",names.len());
+    Ok(())
 }
